7e0b40f48888a0e7f94bae3748b87f33b1dd6f5b
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2025-10-27 20:06:57 -0300

    chore: add auth controller, module and service

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       }),
       inject: [ConfigService],
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
