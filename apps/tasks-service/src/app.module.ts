f6588e5a4b3f095c6e8fbadade4ce137f2879151
Pablo H. F. Roscoche <pablo.dkz2@gmail.com>
2025-10-30 04:05:00 -0300

    feat: add rabbitmq and typeorm connection

--- apps/tasks-service/src/app.module.ts
+++ apps/tasks-service/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TasksModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.example',
+    }),
+
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        entities: [],
+        migrations: ['dist/migrations/*.js'],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
